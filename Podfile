e5c1a8cb7b043f79f72fda903916c84cc67f3975
ash <aichiko66@163.com>
2019-03-01 10:03:15 +0800

    Initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'moya_demo' do
+platform :ios, '8.0'
+use_frameworks!
+
+pod 'Moya', '~> 12.0'
+# or 
+pod 'Moya/RxSwift', '~> 12.0'
+
+end
---
+++ moya_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  moya_demo
+//
+//  Created by 赵光飞 on 2019/2/28.
+//  Copyright © 2019 ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
